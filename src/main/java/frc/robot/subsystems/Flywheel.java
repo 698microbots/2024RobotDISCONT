4817a3e830684b13779b574f9a134f3fe184a90a
mskirthik <46059837+mskirthik@users.noreply.github.com>
2024-01-16 14:31:28 -0700

    rework the Flywheel subsystem

--- src/main/java/frc/robot/subsystems/Flywheel.java
+++ src/main/java/frc/robot/subsystems/Flywheel.java
@@ -1,29 +1,19 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Flywheel extends SubsystemBase {
+public class Flywheel extends SubsystemBase{
+    private final TalonFX motor = new TalonFX(0); // create a new motor object
 
-  private final TalonFX motor = new TalonFX(0);
-  
+    // constructor
+    public Flywheel(){
 
-  /** Creates a new DriveTrain. */
-  public Flywheel() {
-  }
+    }
 
-  public void setFlywheelMotorSpeed(double speed) {
-    motor.set(ControlMode.PercentOutput, speed);
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    // sets the motor speed for the Flywheel
+    public void setFlywheelMotorSpeed(double speed) {
+        motor.set(speed);
+        }
 }
